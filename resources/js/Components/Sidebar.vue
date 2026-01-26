bfcbd124f2943b58bc120ed67b22570b9e7e8974
jyantikumar <jyantiaustriakumar@gmail.com>
2026-01-26 22:12:02 +0800

    first commit

---
+++ resources/js/Components/Sidebar.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref } from 'vue';
+import { 
+  Home, 
+  LayoutDashboard, 
+  Settings, 
+  Users, 
+  ChevronLeft, 
+  ChevronRight,
+  LogOut 
+} from 'lucide-vue-next';
+
+const isCollapsed = ref(false);
+
+const navItems = [
+  { name: 'Dashboard', icon: LayoutDashboard, href: '#' },
+  { name: 'RFP', icon: null, href: '#' },
+  { name: 'Petty Cash', icon: null, href: '#' },
+  { name: 'Liquidation', icon: null, href: '#' },
+  { name: 'Reports', icon: null, href: '#' },
+];
+</script>
+
+<template>
+  <aside 
+  :class="[
+    'h-screen bg-[#515050] text-[#dcdcdc] transition-all duration-300 flex flex-col py-4',
+    isCollapsed ? 'w-20' : 'w-64'
+  ]"
+>
+  <div class="flex items-center justify-between mb-8 px-4">
+    <div v-if="!isCollapsed" class="font-bold text-xl text-white truncate">
+      Elite<span class="text-[#87af49]"> Boogsh</span>
+    </div>
+    <button 
+      @click="isCollapsed = !isCollapsed" 
+      class="p-2 rounded-lg hover:bg-[#dcdcdc] hover:text-[#515050] transition-colors"
+    >
+      <ChevronLeft v-if="!isCollapsed" :size="20" />
+      <ChevronRight v-else :size="20" />
+    </button>
+  </div>
+
+    <nav class="flex-1 space-y-2">
+    <a 
+        v-for="item in navItems" 
+        :key="item.name" 
+        :href="item.href"
+        class="flex items-center w-52 hover:bg-[#dcdcdc] hover:text-[#515050] p-3 px-4 rounded-tr-xl rounded-br-xl transition-all group"
+    >
+        <component :is="item.icon" :size="24" />
+        <span 
+        v-if="!isCollapsed" 
+        class="ml-4 font-medium transition-opacity duration-200"
+        >
+        {{ item.name }}
+        </span>
+    </a>
+    </nav>
+
+  <div class="pt-4 border-t border-slate-800 px-4">
+    <button class="flex items-center w-full p-3 rounded-xl hover:bg-red-500/10 hover:text-red-500 transition-colors">
+      <LogOut :size="24" />
+      <span v-if="!isCollapsed" class="ml-4 font-medium">Logout</span>
+    </button>
+  </div>
+</aside>
+</template>
